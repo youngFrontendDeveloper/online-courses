fe5831832f6f9245d67a6bbaf690ad7445714c6a
Nina <frontend-developer@inbox.ru>
2025-12-08 13:32:57 +0300

    Улучшения в код

--- index.js
+++ index.js
@@ -1,93 +1,97 @@
 import { categories, courses } from "./constants.js";
 
-
-const filter = document.querySelector(".filter");
-const button = document.querySelector(".search-form__button");
-let filteredCourses = courses;
+const filter = document.querySelector( ".filter" );
+const searchButton = document.querySelector( ".search-form__button" );
+const coursesList = document.querySelector( ".courses-list" );
+let filteredCourses = [ ...courses ];
 
 // Добавляем элементы в фильтр
 filter.innerHTML = categories
   .map(
     (category, index) => `
-      <li class="filter__item${index === 0 ? " active" : ""}">
-        <span  class="filter__title">${category.title}
-          <span class="filter__counter">${category.count}</span>
+      <li class="filter__item${ index === 0 ? " active" : "" }" data-category="${ category.title }">
+        <span  class="filter__title">${ category.title }
+          <span class="filter__counter">${ category.count }</span>
         </span>
       </li>
     `
   )
-  .join("");
+  .join( "" );
 
 //Добавляем карточки
 const renderCourses = (list) => {
-  const coursesList = document.querySelector(".courses-list");
-
   coursesList.innerHTML = list
-    .map(course => `
-  <li class="courses-list__item course">
-    <img src=${course.image} alt=${course.title} class="course__image">
-    <div class="course__info-block">
-      <span class="course__badge" style="background-color:${categories.find(category => category.title === course.category)["bg-color"]}">${course.category}</span>
-      <h3 class="course__title">${course.title}</h3>
-    <div class="course__info">
-      <span  class="course__price">$${course.price}</span>
-      <span  class="course__author">by ${course.author}</span>
-     </div>
-    </div> 
-  </li>
+    .map(
+      (course) => `
+        <li class="courses-list__item course">
+          <img src=${ course.image } alt=${ course.title } class="course__image">
+          <div class="course__info-block">
+            <span class="course__badge" style="background-color:${
+        categories.find( (category) => category.title === course.category )[
+          "bg-color"
+          ]
+      }">${ course.category }</span>
+            <h3 class="course__title">${ course.title }</h3>
+          <div class="course__info">
+            <span  class="course__price">$${ course.price }</span>
+            <span  class="course__author">by ${ course.author }</span>
+           </div>
+          </div> 
+        </li>
   `
-    ).join("");
-}
-
-renderCourses(filteredCourses);
-
-const addActiveClass = (event, el) => {
-  const filterItem = event.target.closest(el);
-  if (!filterItem) {
-    return;
-  }
-  document.querySelectorAll(el).forEach((item) => {
-    if (item.classList.contains("active")) {
-      item.classList.remove("active");
-    }
-  });
-
-  filterItem.classList.add("active");
+    )
+    .join( "" );
 };
 
-const filterByCategory = (event) => {
-  const category = event.target.textContent;
+renderCourses( filteredCourses );
 
-  if (category.includes("All")) {
+const addActiveClass = (clickedItem, selector) => {
+  document.querySelector( `${ selector }.active` )?.classList.remove( "active" );
+  clickedItem.classList.add( "active" );
+};
+
+// фильтрация курсов по категориям
+const filterByCategory = (category) => {
+
+  if( category === "All" ) {
     filteredCourses = courses;
   } else {
-    filteredCourses = courses.filter((course) => category.includes(course.category));
+    filteredCourses = courses.filter( (course) => course.category === category );
   }
 
-  renderCourses(filteredCourses);
-}
+  renderCourses( filteredCourses );
+};
 
-filter.addEventListener("click", (event) => {
-  addActiveClass(event, ".filter__item");
-  filterByCategory(event);
-});
+filter.addEventListener( "click", (event) => {
+  const filterItem = event.target.closest( ".filter__item" );
+
+  if( !filterItem ) return;
+
+  addActiveClass( filterItem, ".filter__item" );
+  filterByCategory( filterItem.dataset.category );
+} );
 
 // Ищем курс
 const searchCourse = (str) => {
-  filteredCourses = courses.filter((course) => course.title.toLowerCase().includes(str.toLowerCase()));
-  renderCourses(filteredCourses);
-}
+  filteredCourses = courses.filter( (course) =>
+    course.title.toLowerCase().includes( str.toLowerCase() )
+  );
 
-
-button.addEventListener("click", (event) => {
-  event.preventDefault();
-  const value = document.querySelector(".search-form__input").value;
-
-  if (value) {
-    searchCourse(value);
+  if( filteredCourses.length ) {
+    renderCourses( filteredCourses );
+  } else {
+    coursesList.innerHTML = `
+      <p>Такие курсы не найдены</p>
+    `;
   }
-})
 
+};
 
+searchButton.addEventListener( "click", (event) => {
+  event.preventDefault();
+  const value = document.querySelector( ".search-form__input" ).value;
 
-
+  if( value ) {
+    searchCourse( value );
+  }
+} );
